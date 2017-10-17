9f57c8bde5b4ca5c7c6c6059c4e4048b99cc5cb3
erikwiklander <erik.wiklander@wiklandia.com>
2017-10-17 21:52:02 +0200

    First commit

---
+++ src/main/java/io/wiklandia/tramapi/TramApiApplication.java
@@ -0,0 +1,12 @@
+package io.wiklandia.tramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramApiApplication.class, args);
+	}
+}
---
+++ src/test/java/io/wiklandia/tramapi/TramApiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.wiklandia.tramapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TramApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
