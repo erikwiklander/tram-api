278403b298e292f9630d90047b7d2e965b203430
erikwiklander <erik.wiklander@wiklandia.com>
2017-10-18 15:33:58 +0200

    Fix so that SSL is require in prod

--- src/main/java/io/wiklandia/tramapi/WebSecurityConfig.java
+++ src/main/java/io/wiklandia/tramapi/WebSecurityConfig.java
@@ -1,16 +1,26 @@
 package io.wiklandia.tramapi;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.AllArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final SecurityProperties securityProperties;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/application/**", "/station/**").permitAll().anyRequest().authenticated();
+
+		if (securityProperties.isRequireSsl()) {
+			http.requiresChannel().anyRequest().requiresSecure();
+		}
 	}
 }
