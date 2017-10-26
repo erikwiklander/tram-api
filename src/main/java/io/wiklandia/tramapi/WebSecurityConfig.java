8d884465003bd73582dc25674c8557dbc6076b99
erikwiklander <erik.wiklander@wiklandia.com>
2017-10-26 09:46:42 +0200

    Do not create a session

--- src/main/java/io/wiklandia/tramapi/WebSecurityConfig.java
+++ src/main/java/io/wiklandia/tramapi/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,6 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.httpBasic();
+
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.cors();
 
