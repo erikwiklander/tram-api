821ec8ea1ffd5d2f92597685eb75256ff107107e
erikwiklander <erik.wiklander@wiklandia.com>
2017-11-20 21:45:52 +0100

    Added nästa färja.se

--- src/main/java/io/wiklandia/tramapi/WebSecurityConfig.java
+++ src/main/java/io/wiklandia/tramapi/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().mvcMatchers("/cloudfoundryapplication/**").permitAll();
 
 		// @formatter:off
 		http.authorizeRequests()
@@ -56,6 +59,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/cloudfoundryapplication/**");
+		web.ignoring().mvcMatchers("/cloudfoundryapplication/**");
 	}
 
 }
